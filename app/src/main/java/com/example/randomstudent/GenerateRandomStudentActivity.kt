5956f4108e6f9cc64a038ec77430f381ca199a44
Natasha Laurie <natusik711@gmail.com>
2021-12-25 21:17:08 +0300

    feat: add random student app

---
+++ app/src/main/java/com/example/randomstudent/GenerateRandomStudentActivity.kt
@@ -0,0 +1,58 @@
+package com.example.randomstudent
+
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+private const val EXTRA_STUDENTS_ARRAYLIST =
+    "com.example.randomstudent.EXTRA_STUDENTS_ARRAYLIST"
+
+class GenerateRandomStudentActivity : AppCompatActivity() {
+
+    private var students = ArrayList<String>()
+
+    private lateinit var generateButton: Button
+    private lateinit var backButton: Button
+    private lateinit var studentTextView: TextView
+    private lateinit var pic: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_generate_random_student)
+
+        students = intent.getStringArrayListExtra(EXTRA_STUDENTS_ARRAYLIST) as ArrayList<String>
+        //println(students)
+
+        generateButton = findViewById(R.id.buttonGenerate)
+        studentTextView = findViewById(R.id.textViewRandomStudent)
+        pic = findViewById(R.id.imageView)
+
+        generateButton.setOnClickListener {
+            val randomStudent = students.shuffled().first()
+            studentTextView.text = randomStudent
+            val mDrawableName = randomStudent.lowercase()
+            val resID: Int = pic.getContext().getResources()
+                .getIdentifier(mDrawableName, "drawable", pic.getContext().getPackageName())
+            pic.setImageResource(resID)
+        }
+
+        backButton = findViewById(R.id.button2)
+        backButton.setOnClickListener{
+            this.finish()
+        }
+
+    }
+
+    companion object {
+        fun newIntent(packageContext: Context, students: ArrayList<String>): Intent {
+            return Intent(packageContext, GenerateRandomStudentActivity::class.java).apply {
+                putExtra(EXTRA_STUDENTS_ARRAYLIST, students)
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/randomstudent/MainActivity.kt
+++ app/src/main/java/com/example/randomstudent/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.randomstudent
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.CheckBox
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var okButton: Button
+    var students = mutableSetOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        okButton = findViewById<Button>(R.id.buttonOk)
+        okButton.setOnClickListener {
+
+            val studentsArr : ArrayList<String> = ArrayList<String>()
+            studentsArr.addAll(students)
+            println(studentsArr)
+            val intent = GenerateRandomStudentActivity.newIntent(this@MainActivity, studentsArr)
+            startActivity(intent)
+        }
     }
+
+    fun onCheckboxClicked(view: android.view.View) {
+        if (view is CheckBox) {
+            val checked: Boolean = view.isChecked
+            if (checked) {
+                students.add((view.text).toString())
+                /*for (item in students) {
+                    println(item)
+                }*/
+            } else {
+                students.remove((view.text).toString())
+                /*for (item in students) {
+                    println(item)
+                }*/
+            }
+            //println(students.toTypedArray().contentToString())
+        }
+    }
+
 }
